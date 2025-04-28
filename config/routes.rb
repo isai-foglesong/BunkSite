f2e366aaab5f36ecc52beb0136ad2c6efeb04c65
isai-foglesong <71203580+isai-foglesong@users.noreply.github.com>
2025-04-28 00:46:07 -0700

    Fix ActionCable production config

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-end
+
+  
+  Rails.application.routes.draw do
+    root to: "application#hello"
+  end
